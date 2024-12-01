de9f2b9211c0fe302a4caa2b8be2ab11192b4a8d
Alexandre Carmo <alecarmo1980@gmail.com>
2024-12-01 12:00:50 -0300

    estrutura para criar alguns exemplos usando swiftui 6

---
+++ SwfitUI6_Conversor/SwfitUI6_Conversor/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  SwfitUI6_Conversor
+//
+//  Created by Alexandre C do Carmo on 01/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            //background image
+            VStack {
+                // Prancing pony image
+                
+                //Currency exchange text
+                
+                //Conversion section
+                HStack {
+                    // Left conversion saction
+                    VStack {
+                        //Currecy
+                        HStack {
+                            //Currecy image
+                            
+                            //Currecy text
+                        }
+                        
+                        //Text field
+                    }
+                    
+                    // Equal sign
+                    
+                    // Right conversion section
+                    VStack {
+                        //Currecy
+                        HStack {
+                            //Currecy text
+                            
+                            //Currecy image
+                        }
+                        
+                        //Text field
+                    }
+                    
+                }
+                
+                // Info button
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwfitUI6_Conversor/SwfitUI6_Conversor/SwfitUI6_ConversorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwfitUI6_ConversorApp.swift
+//  SwfitUI6_Conversor
+//
+//  Created by Alexandre C do Carmo on 01/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwfitUI6_ConversorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
